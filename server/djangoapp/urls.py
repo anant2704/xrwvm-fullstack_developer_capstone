f942e68d001e18d8a15f78ecdcb4a413b53c9a09
Anant Joshi <anant.joshi2704@gmail.com>
2025-03-30 19:59:51 +0530

    Car Models added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,12 +7,12 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path('register/', views.registration, name='registration'),
+    path('register', views.registration, name='registration'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
     path(route='logout', view=views.logout_request, name='logout'),
-
+    path(route='get_cars', view=views.get_cars, name ='get_cars'),
     # path for dealer reviews view
 
     # path for add a review view
